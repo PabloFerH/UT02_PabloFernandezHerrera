a3cf69aea934a4c68367f202506bf7c96458d7f1
PabloFerH <pfer9504@gmail.com>
2017-12-18 19:13:20 +0000

    Commit inicial

---
+++ src/com/cip/prog/Imprimir_Pantalla.java
@@ -0,0 +1,13 @@
+package com.cip.prog;
+
+public class Imprimir_Pantalla {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bienvenido al sistema\r\n" + 
+				"Hoy es lunes\r\n" + 
+				"Son las 08:00\r\n" + 
+				"Tiene 8 correos sin leer");
+	}
+
+}
